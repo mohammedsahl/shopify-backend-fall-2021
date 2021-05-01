6513f0115752889143d5c6b54c6292fbdfd0a12f
Mohammed Sahl <mskably@uwaterloo.ca>
2021-05-01 19:26:35 -0400

    Fix upload page, file upload bug and formatting

--- api/controllers/ImagesController.js
+++ api/controllers/ImagesController.js
@@ -13,27 +13,43 @@ module.exports = {
         status: "GET not allowed",
       });
     }
-    var uploadFile = req.file("image");
+
+    // Get uploaded file
+    const uploadFile = req.file("image");
+
+    //Upload and process filfe
     uploadFile.upload({ dirname: process.cwd() + '/assets/images/uploads/' }, function whenDone(err, files) {
+
+      // Send error if needed
       if (err) return res.send(500,err);
 
+      // Iterate through files
       for (const file of files) {
+
+        // Get filename and directories for temp storage and then upload
         const filename = file.fd.substring(file.fd.lastIndexOf('\\') + 1)
         const uploadLocation = process.cwd() +'/assets/images/uploads/' + filename;
-        const tempLocation = process.cwd() + '/.tmp/public/images/uploads/' + filename;
-        fs.createReadStream(uploadLocation).pipe(fs.createWriteStream(tempLocation));
+        const tmpLocation = process.cwd() + '/.tmp/public/images/uploads/' + filename;
+        fs.createReadStream(uploadLocation).pipe(fs.createWriteStream(tmpLocation));
+
+        // Create new image record
         Images.create({
           imageUploadFileDirectory: file.fd,
           imageFilename: file.fd.replace(/^.*[\\\/]/, ''),
           imageUploadMime: file.type,
-          imageTitle: file.filename,
-          imageTags: ["test"],
+          imageTitle: req.body.imageTitle,
+          imageTags: req.body.imageTags.split(','),
         }).exec(function (err) {
           if (err) return res.serverError(err);
         });
       }
+
+      // Go back to list page
       res.redirect('/images/list')
     });
+
+    //Good practice
+    return false;
   },
 
   get: function (req, res) {
@@ -41,6 +57,9 @@ module.exports = {
       if (err) res.send(500, {error: err})
       res.view('pages/list', {images: images})
     })
+
+    //Good practice
+    return false;
   },
 
   delete: function (req, res) {
@@ -59,6 +78,8 @@ module.exports = {
         res.redirect('/images/list')
       })
     });
+
+    //Good practice
     return false
   }
 };
